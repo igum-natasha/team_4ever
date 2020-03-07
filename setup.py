f3ae23989dc3008195b4924a1f01418cafd1d8b6
igum-natasha <igum.natasha@gmail.com>
2020-03-07 17:20:25 +0300

    работа на занятии

---
+++ setup.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+TOKEN = '1003399173:AAFkssy_8OxBxpbCP9JxH-C6nMG53T_Pvrg'  # insert your token here
+PROXY = 'https://telegg.ru/orig/bot'  # proxy to connect against telegram ban
