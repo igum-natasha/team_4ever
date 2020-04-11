569bff6bdf658dd05fffa20cfba84a575920db9d
igum-natasha <igum.natasha@gmail.com>
2020-04-11 16:21:08 +0300

    Correction of defects in classes.py and add /cor

--- chat_bot_template1.py
+++ chat_bot_template1.py
@@ -94,36 +94,60 @@ def facts(update: Updater, context: CallbackContext):
             ma = all[i]['upvotes']
             update.message.reply_text(f'User: {all[i]["user"]["name"]["first"]} {all[i]["user"]["name"]["last"]}\n{all[i]["text"]}\nLikes: {all[i]["upvotes"]}')
 
+@decorator_error
+@analise
 def corona(update: Updater, context: CallbackContext):
     answer = 'Пять провинций с наибольшим кол-вом зараженных COVID-19:\n'
-    prov={}
-    count=[]
-    WorkWithCoronaData.provinces(prov,count)
-    for elem in count[:5]:
-        for key, value in prov.items():
+    corona=WorkWithCoronaData
+    corona.prov={}
+    corona.count=[]
+    corona.provinces()
+    for elem in corona.count[:5]:
+        for key, value in corona.prov.items():
             if value == elem:
                 answer += f"{key} : {value}\n"
     update.message.reply_text(answer)
 
+@decorator_error
+@analise
 def corona_dynamics(update: Updater, context: CallbackContext):
     answer='Динамика заражений COVID-19 за два дня для Топ-5 стран:\n'
-    count,count1=[0]*1000,[0]*1000
-    now=WorkWithCoronaData.corona_dynamics(0,count)
-    now1=WorkWithCoronaData.corona_dynamics(1,count1)
-    for elem in count[:5]:
-        for key,value in now.items():
-            for key1,value1 in now1.items():
+    corona = WorkWithCoronaData
+    corona1=WorkWithCoronaData
+    corona.count,corona1.count1=[0]*1000,[0]*1000
+    now=WorkWithCoronaData.corona_dynamics(0)
+    now1=WorkWithCoronaData.corona_dynamics(1)
+    for elem in corona.count[:5]:
+        for key,value in corona.now.items():
+            for key1,value1 in corona1.now1.items():
                 if key==key1 and value[4]==elem:
                     answer+=f'{str(value[0]).upper()}\n'
                     answer+=f'Confirmed: {value[1]-value1[1]} Deaths: {value[2]-value1[2]} Recovered: {value[3]-value1[3]} Active: {value[4]-value1[4]}\n'
     update.message.reply_text(answer)
 
+@decorator_error
+@analise
+def corona_russia(update: Updater, context: CallbackContext):
+    answer='Динамика заражений COVID-19 за два дня для России:\n'
+    corona = WorkWithCoronaData
+    corona1 = WorkWithCoronaData
+    corona.count, corona1.count1 = [0] * 1000, [0] * 1000
+    now = WorkWithCoronaData.corona_dynamics(0)
+    now1 = WorkWithCoronaData.corona_dynamics(1)
+    for key,value in corona.now.items():
+            for key1,value1 in corona1.now1.items():
+                    answer+=f'{str(value[0]).upper()}\n'
+                    answer+=f'Confirmed: {value[1]-value1[1]} Deaths: {value[2]-value1[2]} Recovered: {value[3]-value1[3]} Active: {value[4]-value1[4]}\n'
+    update.message.reply_text(answer)
 
+@decorator_error
+@analise
 def info(update: Updater, context: CallbackContext):
     answer='Команды для работы с ботом:\n/start-Приветствие\n/help-Подсказка с чего начать\n' \
            '/history-История последних запросов\n/facts-Самый популярный факт о котах\n' \
            '/corona_stats-Пять провинций с наибольшим кол-вом зараженных\n ' \
-           '/corona_dynamics-Динамика заражений COVID-19 за два дня для Топ-5 стран\n'
+           '/corona_dynamics-Динамика заражений COVID-19 за два дня для Топ-5 стран\n' \
+           '/corona_russia-Динамика заражения COVID-19 за два дня для России\n'
     update.message.reply_text(answer)
 
 def main():
@@ -140,6 +164,7 @@ def main():
     updater.dispatcher.add_handler(CommandHandler('facts', facts))
     updater.dispatcher.add_handler(CommandHandler('corona_stats', corona))
     updater.dispatcher.add_handler(CommandHandler('corona_dynamics', corona_dynamics))
+    updater.dispatcher.add_handler(CommandHandler('corona_russia', corona_russia))
     updater.dispatcher.add_handler(CommandHandler('info', info))
     # on noncommand i.e message - echo the message on Telegram
     updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
--- classes.py
+++ classes.py
@@ -2,95 +2,108 @@ import csv
 import requests
 import datetime
 class WorkWithCsvTable():
-    @staticmethod
-    def write_table(file_name):
-        data=[]
+    def __init__(self):
+        self.data=[]
+    def write_table(self,file_name):
         with open(file_name, 'wb') as file:
             writer = csv.DictWriter(file)
             writer.writeheader()
-            for row in data:
+            for row in self.data:
                 writer.writerow(row)
 
-    @staticmethod
-    def read_table(file_name):
-        data=[]
+    def read_table(self,file_name):
         with open(file_name, 'r') as file:
             table=csv.DictReader(file)
             for row in table:
                 array = {}
                 for key in row.keys():
                     array[key] = row[key]
-                data.append(array)
-        return data
+                self.data.append(array)
+    def get_data(self):
+        return self.data
 
 class WorkWithCoronaData(WorkWithCsvTable):
-    @staticmethod
-    def get_table(data):
-        data1 = datetime.date.today()
-        data1 = data1.strftime("%m-%d-%Y").split('-')
+    def __init__(self,data):
+        super().__init__(data)
+        self.prov={}
+        self.count=[]
+        self.data1=[]
+        self.table=[]
+        self.now={}
+    def get_table(self,day):
+        self.data1 = datetime.date.today()
+        self.data1 = self.data1.strftime("%m-%d-%Y").split('-')
         while True:
-            url = f'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{data1[0]}-{data1[1]}-{data1[2]}.csv'
+            url = f'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{self.data1[0]}-{self.data1[1]}-{self.data1[2]}.csv'
             r = requests.get(url, allow_redirects=True)
             if r.status_code != 200:
-                if int(data1[1]) < 10:
-                    data1[1] = '0' + str(int(data1[1]) - 1-data)
+                if int(self.data1[1]) < 10:
+                    self.data1[1] = '0' + str(int(self.data1[1]) - 1-day)
                 else:
-                    data1[1] = str(int(data1[1]) - 1-data)
+                    self.data1[1] = str(int(self.data1[1]) - 1-day)
             else:
                 break
         corona = open('google.csv', 'wb')
         corona.write(r.content)
         corona.close()
-        table=WorkWithCsvTable.read_table('google.csv')
-        if int(data1[1]) < 10:
-            data1[1] = '0' + str(int(data1[1]) - 1)
+        WorkWithCsvTable.read_table('google.csv')
+        self.table=WorkWithCsvTable.get_data
+        if int(self.data1[1]) < 10:
+            self.data1[1] = '0' + str(int(self.data1[1]) - 1)
         else:
-            data1[1] = str(int(data1[1]) - 1)
-        return data1
+            self.data1[1] = str(int(self.data1[1]) - 1)
 
-    @staticmethod
-    def provinces(prov,count):
-        WorkWithCoronaData.get_table()
-        table=WorkWithCsvTable.read_table('google.csv')
-        for row in table:
+
+    def provinces(self):
+        WorkWithCoronaData.get_table(0)
+        for row in self.table:
              if int(row['Active']) != 0:
                 if row['Province_State'] != '':
-                    prov[f"{row['Province_State']}"] = int(row['Active'])
+                    self.prov[f"{row['Province_State']}"] = int(row['Active'])
                 else:
-                    prov[f"{row['Country_Region']}"] = int(row['Active'])
-                count.append(int(row['Active']))
-        count.sort(reverse=True)
+                    self.prov[f"{row['Country_Region']}"] = int(row['Active'])
+                self.count.append(int(row['Active']))
+        self.count.sort(reverse=True)
 
-    @staticmethod
-    def corona_dynamics(data,count):
-        WorkWithCoronaData.get_table(data)
-        table = WorkWithCsvTable.read_table('google.csv')
-        now={}
+    def corona_dynamics(self,day):
+        WorkWithCoronaData.get_table(day)
         k=0
         buf=[]
-        for row in table:
+        for row in self.table:
             if int(row['Active'])!=0  and row["Country_Region"] not in buf:
                 buf.append(row["Country_Region"])
-                now[k]=[row["Country_Region"],
+                self.now[k]=[row["Country_Region"],
                  int(row['Confirmed']),
                  int(row['Deaths']),
                  int(row['Recovered']),
                  int(row["Active"])]
-                count[k]=int(row['Active'])
+                self.count[k]=int(row['Active'])
                 k += 1
             elif int(row['Active'])!=0 and row["Country_Region"] in buf:
-                for key,value in now.items():
+                for key,value in self.now.items():
                     if value[0]==row["Country_Region"]:
-                        now[key] = [row["Country_Region"],
+                        self.now[key] = [row["Country_Region"],
                                   value[1]+int(row['Confirmed']),
                                   value[2]+int(row['Deaths']),
                                   value[3]+int(row['Recovered']),
                                   value[4]+int(row["Active"])]
-                        count[key] = value[4]+int(row["Active"])
+                        self.count[key] = value[4]+int(row["Active"])
+        self.count.sort(reverse=True)
+
+    def corona_russia(self,day):
+        WorkWithCoronaData.get_table(day)
+        k = 0
+        buf = []
+        for row in self.table:
+            if row["Country_Region"]=="Russia":
+                buf.append(row["Country_Region"])
+                self.now[k] = [row["Country_Region"],
+                          int(row['Confirmed']),
+                          int(row['Deaths']),
+                          int(row['Recovered']),
+                          int(row["Active"])]
 
 
-        count.sort(reverse=True)
-        return now
 
 class Website():
     @staticmethod
